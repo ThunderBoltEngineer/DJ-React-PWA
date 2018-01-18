fff1da82da2b050b07c6ece49053b1f829531772
Andrew Sydor <thunderboltengineer@gmail.com>
2018-01-18 14:38:31 +0200

    facebook, twitter, google authentication

--- src/app/components/login/login.js
+++ src/app/components/login/login.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import firebase from 'firebase';
+import axios from 'axios';
 
 // import css
 import * as styles from "./styles.css";
@@ -9,6 +11,17 @@ import FacebookButton from "src/resources/images/login/fb_signin.png";
 import TwitterButton from "src/resources/images/login/tw_signin.png";
 import GooglePlusButton from "src/resources/images/login/gp_signin.png";
 
+// import firebase configuration info
+import config from "src/config/firebase_config.js";
+const firebaseConfig = config.config;
+
+// configure firebase 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth;
+const fbAuthProvider = new firebase.auth.FacebookAuthProvider();
+const twitterAuthProvider = new firebase.auth.TwitterAuthProvider();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 
 // component
@@ -17,6 +30,46 @@ export default class Login extends Component {
 
       componentDidMount() {
             console.log("component mount!");
+      }
+
+      /*
+            firebase authentication with given provider, get access token
+
+            */
+
+      signInWithProvider(provider) {
+
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                  var token = result.credential.accessToken;
+                  var user = result.user;
+
+                  console.log(token);
+
+                  // get party dict  (this should be built as independent module later)
+                  let baseUrl = "https://api-next.spinfluence.live/api/v1";
+                  let apiUrl = "/parties/join/TEST";
+                  let bearerToken = "Bearer " + token;
+  
+                  axios({
+                        method: 'post',
+                        url: baseUrl + apiUrl,
+                        headers: ['AUTHORIZATION': bearerToken]
+                  }).then(function(response) {
+
+                        console.log(response);
+
+                  }).catch(function(error) {
+
+                        // output error message
+                        var errorMessage = error.message;
+                        console.log(errorMessage);
+                  });
+ 
+            }).catch(function(error) {
+                  // output error message
+                  var errorMessage = error.message;
+                  console.log(errorMessage);
+            });
       }
 
 
@@ -28,16 +81,19 @@ export default class Login extends Component {
       handleFBLogin(e) {
             console.log("facebook button clicked!");
 
+            this.signInWithProvider(fbAuthProvider);
       }
 
       handleTwitterLogin(e) {
             console.log("twitter button clicked!");
 
+            this.signInWithProvider(twitterAuthProvider);
       }
 
       handleGooglePlusLogin(e) {
             console.log("google plus button clicked");
 
+            this.signInWithProvider(googleAuthProvider);
       }
 
 
@@ -72,7 +128,7 @@ export default class Login extends Component {
             		</div >
             		<div className={`${styles["div-fb"]}`}>
 
-            			<div className={`${styles["btn-social"]}`} onClick={this.handleFBLogin}>
+            			<div className={`${styles["btn-social"]}`} onClick={this.handleFBLogin.bind(this)}>
             				<img className="img-responsive" src={FacebookButton}/>
             			</div>
             			<div className={`${styles["div-or-separator"]}`}>
@@ -81,18 +137,18 @@ export default class Login extends Component {
             		</div>
             		<div className={`${styles["div-other"]}`}>
             			<div className={`${styles["div-other-half"]}`}>
-            				<div className={`${styles["btn-social"]}`} onClick={this.handleTwitterLogin}>
+            				<div className={`${styles["btn-social"]}`} onClick={this.handleTwitterLogin.bind(this)}>
             					<img className="img-responsive" src={TwitterButton}/>
             				</div>
             			</div>
             			<div className={`${styles["div-other-half"]}`}>
-            				<div className={`${styles["btn-social"]}`} onClick={this.handleGooglePlusLogin}>
+            				<div className={`${styles["btn-social"]}`} onClick={this.handleGooglePlusLogin.bind(this)}>
             					<img className="img-responsive" src={GooglePlusButton}/>
             				</div>
             			</div>
             		</div>
             		<div className={`${styles["div-skip"]}`}>
-            			<div className={`${styles["btn-transparent"]} ${styles["btn-skip"]}`} onClick={this.handleSkip}>
+            			<div className={`${styles["btn-transparent"]} ${styles["btn-skip"]}`} onClick={this.handleSkip.bind(this)}>
             				Skip Login
             			</div>
             		</div>
---
+++ src/config/firebase_config.js
@@ -0,0 +1,12 @@
+// firebase configuration
+
+export default {
+	config: {
+		apiKey: "AIzaSyCKO0ZkKAemKMSnGeq1K192y8pemWfvZNE",
+    	authDomain: "spinfluence-320d8.firebaseapp.com",
+    	databaseURL: "https://spinfluence-320d8.firebaseio.com",
+    	projectId: "spinfluence-320d8",
+    	storageBucket: "spinfluence-320d8.appspot.com",
+    	messagingSenderId: "133793994050"
+	}
+}
